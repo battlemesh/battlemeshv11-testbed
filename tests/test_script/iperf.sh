cf7e5155023918ca6cc3518755a99ac15a9f51e5
Leonardo Maccari <leonardo.maccari@unitn.it>
2018-05-09 16:43:00 +0200

    new parser for netjson

--- tests/test_script/iperf.sh
+++ tests/test_script/iperf.sh
@@ -1,14 +1,18 @@
 # perform an iperf towards a node, save the result in a file in /tmp 
-
+source ip_list.sh
 usage() { echo "$0 usage:" && grep " .)\ #" $0; exit 0; }
 [ $# -eq 0 ] && usage
-while getopts ":c:n" arg; do
+
+while getopts ":c:nr" arg; do
   case $arg in
     c) # Specify destination.
       DEST=${OPTARG}
       ;;
-    n) 
+    n) # delete previous output file
 	  DELETE=1
+      ;;
+    r) # pick a random destination
+	  RND=1
       ;;
     h | *) # Display help.
       usage
@@ -23,5 +27,13 @@ then
     rm $IPERFOUT
 fi 
 
+if [ $RND ];
+then 
+    DEST_ID=$(($RANDOM%${#IPv4[@]}))
+    DEST=${IPv4[$DEST_ID]}
+fi
 
-iperf -f M -y C -t 10 -e -V -c $DEST | tail -n 2 >> $IPERFOUT
+
+
+#iperf -f M -y C -t 10 -e -V -c $DEST | tail -n 2 >> $IPERFOUT
+iperf -f M -y C -t 10 -e -c $DEST | tail -n 2 >> $IPERFOUT
