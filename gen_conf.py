b72f1095f179579c6bf8f11a6b26c73fecac04fb
Gabriele Gemmi <gabriel@autistici.org>
2018-05-09 19:07:14 +0200

    updates

--- gen_conf.py
+++ gen_conf.py
@@ -2,20 +2,24 @@
 import os
 with open("config-wdr/network.template", "r") as f_temp:
     template = f_temp.read()
-    for i in range(12, 21):
-        mgmt_ip = "192.168.254.%d" % (i)
-        lan_ip = "10.1.%d.1" % (i)
-        radio_ip = "10.0.%d.1" % (i)
-        print(mgmt_ip)
-        if not os.path.exists("config-wdr/" + mgmt_ip):
-            os.makedirs("config-wdr/" + mgmt_ip)
-        os.system("cp config-wdr/dhcp config-wdr/%s/dhcp" % (mgmt_ip))
-        os.system("cp config-wdr/olsrd2 config-wdr/%s/olsrd2" % (mgmt_ip))
-        os.system("cp config-wdr/wireless config-wdr/%s/wireless" % (mgmt_ip))
+    with open("config-wdr/olsrd2.template", "r") as f_olsr:
+        olsr2_template = f_olsr.read()
+        for i in range(1, 21):
+            mgmt_ip = "192.168.254.%d" % (i)
+            lan_ip = "10.1.%d.1" % (i)
+            radio_ip = "10.0.%d.1" % (i)
+            hna = "10.1.%d.0/24" % (i)
+            print(hna)
+            if not os.path.exists("config-wdr/" + mgmt_ip):
+                os.makedirs("config-wdr/" + mgmt_ip)
+            os.system("cp config-wdr/dhcp config-wdr/%s/dhcp" % (mgmt_ip))
+            os.system("cp config-wdr/wireless config-wdr/%s/wireless" % (mgmt_ip))
+            with open("config-wdr/%s/olsrd2" % (mgmt_ip), "w") as f:
+                config = olsr2_template.format(hna)
+                f.write(config)
 
-
-        with open("config-wdr/%s/network" % (mgmt_ip), "w") as f:
-            config = template.format(lan_ip, radio_ip, mgmt_ip)
-            f.write(config)
-            
-    
+            with open("config-wdr/%s/network" % (mgmt_ip), "w") as f:
+                config = template.format(lan_ip, radio_ip, mgmt_ip)
+                f.write(config)
+                
+        
