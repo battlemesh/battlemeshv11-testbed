fb9a4cf86753b3b06bb4b56768d69da9cfa40463
Leonardo Maccari <leonardo.maccari@unitn.it>
2018-05-11 16:45:24 +0200

    parser

--- tests/parser_scripts/parse_iperf.py
+++ tests/parser_scripts/parse_iperf.py
@@ -7,15 +7,16 @@ import tempfile
 import argparse
 import os
 
-def compute_percentiles_iperf(ff):
+def compute_percentiles_iperf(ff, source_ip=""):
         f = open(ff)
         bitrate_list = []
         transferred_list = []
         data = defaultdict(dict)
-        s = ""
+        s = source_ip
         for d in f.readlines():
             data = d.split(",")
-            s = data[1]
+            if not s:
+                s = data[1]
             d = data[3]
             bitrate_list.append(float(data[-1][:-1]))
             transferred_list.append(float(data[7]))
@@ -23,58 +24,80 @@ def compute_percentiles_iperf(ff):
             return
         print s, ",", d, ",", np.percentile(bitrate_list, 50), ",",\
               np.percentile(bitrate_list, 10), ",",\
-              np.percentile(bitrate_list,90), ",",\
+              np.percentile(bitrate_list, 90), ",",\
               np.percentile(transferred_list, 50)
 
-def compute_percentiles_ping(ff):
+
+def compute_percentiles_ping(ff, source_ip=""):
         f = open(ff)
         loss_percentage = []
         avg_rtt = []
         data = defaultdict(dict)
-        s = ""
+        s = source_ip
         for d in f.readlines():
             data = d.split(",")
-            s = data[0]
-            d = data[1]
+            if len(data) < 4:
+                return
+            if not s:
+                s = data[0]
             loss_percentage.append(float(data[2]))
+            d = data[1]
             avg_rtt.append(float(data[4].strip("ms")))
         if not s:
             return
         print s, ",", d, ",", np.percentile(loss_percentage, 50)
 
 
-def print_percentiles(file_list):
-    print("source_address, destination_address, transferred_bytes,"
-          "bits_per_second (median), bits_per_second (10perc),"
-          "bits_per_second (90perc)")
+def print_percentiles(file_list, source_ip=""):
+    print_header = True
     for ff in file_list:
         if os.path.basename(ff).find("iperf") is not -1:
-            compute_percentiles_iperf(ff)
+            if print_header:
+                print("source_address, destination_address, transferred_bytes,"
+                      "bits_per_second (median), bits_per_second (10perc),"
+                      "bits_per_second (90perc)")
+                print_header = False
+            compute_percentiles_iperf(ff, source_ip=source_ip)
         else:
-            compute_percentiles_ping(ff)
+            if print_header:
+                print("source_address, destination_address, ping median loss")
+                print_header = False
+            compute_percentiles_ping(ff, source_ip=source_ip)
 
 
-
-
-#def collect_files(ip_list, file_path):
-#    file_list = []
-#    tmpdir = "/tmp/" + os.tempnam()
-#    os.mkdir(temp_folder)
-#    for ip in ip_list:
-#        cmd = "scp " + ip + ":" + file_path + " " + tmpdir
-#        retcode = subprocess.call(cmd, shell=True)
-#        file_list.append(tmpdir + file_path)
-#    return file_list
+def collect_files(ip_list, file_path):
+    file_list = []
+    tmpdir = "/tmp/" + os.tempnam()
+    os.mkdir(temp_folder)
+    for ip in ip_list:
+        cmd = "scp " + ip + ":" + file_path + " " + tmpdir
+        retcode = subprocess.call(cmd, shell=True)
+        file_list.append(tmpdir + file_path)
+    return file_list
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-f", help="pass a path for files to parse, "
                     "will expand to *.csv")
-#parser.add_argument("-t", help="pass a list of IPs to scp files from",
-#                    nargs="+")
-#parser.add_argument("-p", help="pass a path for the file to scp and parse")
+parser.add_argument("-d", help="pass a dir made of dirs in the format:"
+                    "iperf_test_SOURCE_IP/ each of them containing log file")
+parser.add_argument("-t", help="pass a list of IPs to scp files from",
+                    nargs="+")
+parser.add_argument("-p", help="pass a path for the file to scp and parse")
 
 args = parser.parse_args()
+
+if args.d:
+    sub_folder = args.d
+    folders = [x[0] for x in os.walk(sub_folder)]
+    for f in folders[1:]:
+        source_ip = f.split("/")[-1].split("_")[2]
+        data_path = f+"/*csv"
+        file_list = glob.glob(data_path)
+        print_percentiles(file_list, source_ip=source_ip)
+        print ""
+        print ""
+    exit()
 
 if args.f:
     data_path = args.f+"*csv"
