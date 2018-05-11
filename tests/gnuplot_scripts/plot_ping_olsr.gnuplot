1c20f197f7b6c967fbf3cfbcbfaaec21f4d6a02c
Leonardo Maccari <leonardo.maccari@unitn.it>
2018-05-11 18:06:37 +0200

    betterparser

---
+++ tests/gnuplot_scripts/plot_ping_olsr.gnuplot
@@ -0,0 +1,6 @@
+set terminal eps 
+set output "../graphs/ping_results_olsr.eps"
+set key autotitle columnhead
+load "plot_loss.gnuplot"
+
+
--- tests/parser_scripts/parse_iperf.py
+++ tests/parser_scripts/parse_iperf.py
@@ -21,10 +21,10 @@ def compute_percentiles_iperf(ff, source_ip=""):
             bitrate_list.append(float(data[-1][:-1]))
             transferred_list.append(float(data[7]))
         if not s:
-            return
-        print s, ",", d, ",", np.percentile(bitrate_list, 50), ",",\
-              np.percentile(bitrate_list, 10), ",",\
-              np.percentile(bitrate_list, 90), ",",\
+            return []
+        return s, d, np.percentile(bitrate_list, 50),\
+              np.percentile(bitrate_list, 10),\
+              np.percentile(bitrate_list, 90),\
               np.percentile(transferred_list, 50)
 
 
@@ -40,31 +40,45 @@ def compute_percentiles_ping(ff, source_ip=""):
                 return
             if not s:
                 s = data[0]
-            loss_percentage.append(float(data[2]))
-            d = data[1]
-            avg_rtt.append(float(data[4].strip("ms")))
+            try:
+                loss_percentage.append(float(data[2]))
+                d = data[1]
+                avg_rtt.append(float(data[4].strip("ms")))
+            except Exception:
+                return []
         if not s:
-            return
-        print s, ",", d, ",", np.percentile(loss_percentage, 50),\
-              ",", np.percentile(avg_rtt, 50)
+            return []
+        return s, d, np.percentile(loss_percentage, 50),\
+               np.percentile(avg_rtt, 50)
 
 
 print_header = True
+sort_by = 0
 def print_percentiles(file_list, source_ip=""):
     global print_header
+    global sort_by
+    lines = []
     for ff in file_list:
         if os.path.basename(ff).find("iperf") is not -1:
+            sort_by = 2
             if print_header:
                 print("source_address, destination_address, transferred_bytes,"
                       "bits_per_second (median), bits_per_second (10perc),"
                       "bits_per_second (90perc)")
                 print_header = False
-            compute_percentiles_iperf(ff, source_ip=source_ip)
+            l = compute_percentiles_iperf(ff, source_ip=source_ip)
+            if l:
+                lines.append(l)
         else:
+            sort_by = 3
             if print_header:
                 print("source_address, destination_address, ping median loss")
                 print_header = False
-            compute_percentiles_ping(ff, source_ip=source_ip)
+            l = compute_percentiles_ping(ff, source_ip=source_ip)
+            if l:
+                lines.append(l)
+    return lines
+
 
 
 def collect_files(ip_list, file_path):
@@ -89,20 +103,27 @@ parser.add_argument("-p", help="pass a path for the file to scp and parse")
 
 args = parser.parse_args()
 
+data_lines = []
 if args.d:
     sub_folder = args.d
     folders = [x[0] for x in os.walk(sub_folder)]
     for f in folders[1:]:
-        source_ip = f.split("/")[-1].split("_")[2]
+        try:
+            source_ip = f.split("/")[-1].split("_")[2]
+        except Exception:
+            continue
         data_path = f+"/*csv"
         file_list = glob.glob(data_path)
-        print_percentiles(file_list, source_ip=source_ip)
-    exit()
+        data_lines += print_percentiles(file_list, source_ip=source_ip)
+
+for line in sorted(data_lines, key=lambda x: x[sort_by]):
+    print ",".join(map(str,line))
+exit()
 
 if args.f:
     data_path = args.f+"*csv"
     file_list = glob.glob(data_path)
-    print_percentiles(file_list)
+    data_lines += print_percentiles(file_list)
 elif args.h and args.p:
     file_list = collect_files(args.h)
-    print_percentiles(file_list)
+    data_lines += print_percentiles(file_list)
